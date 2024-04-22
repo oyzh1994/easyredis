124693db99c6e6efdaf3dd7a6aa55a237bae00d7
oyzh <oyzh.1994@qq.com>
2024-04-22 20:30:18 +0800

    1. 新增统计指定键模式数量功能 2. 删除键优化，改用scan批量删除

---
+++ src/main/java/cn/oyzh/easyredis/redis/batch/RedisDeleteResult.java
@@ -0,0 +1,30 @@
+package cn.oyzh.easyredis.redis.batch;
+
+import lombok.Data;
+import redis.clients.jedis.params.ScanParams;
+
+import java.util.Objects;
+
+/**
+ * redis 扫描结果
+ *
+ * @author oyzh
+ * @since 2023/6/28
+ */
+@Data
+public class RedisDeleteResult {
+
+    /**
+     * 光标
+     */
+    private String cursor;
+
+    /**
+     * 数据
+     */
+    private Integer count;
+
+    public boolean isFinish() {
+        return Objects.equals(this.cursor, ScanParams.SCAN_POINTER_START) || count == null || count == 0;
+    }
+}
