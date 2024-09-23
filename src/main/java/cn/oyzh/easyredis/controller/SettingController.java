bd8e3264e86eaf092caea3ae2bdc0bbe2c45d86b
oyzh <oyzh.1994@qq.com>
2024-09-23 18:10:57 +0800

    1. 适配fx-base改造

--- src/main/java/cn/oyzh/easyredis/controller/SettingController.java
+++ src/main/java/cn/oyzh/easyredis/controller/SettingController.java
@@ -200,7 +200,7 @@ public class SettingController extends StageController {
             }
         }
         // 记住页面大小处理
-        if (this.setting.getPageInfo() != null) {
+        if (this.setting.getRememberPageSize() != null) {
             this.pageSize.setSelected(this.setting.isRememberPageSize());
         }
         // 记住页面拉伸处理
@@ -260,7 +260,7 @@ public class SettingController extends StageController {
         // 透明度相关处理
         this.setting.setOpacity(this.opacity.getValue());
         // 其他设置
-        this.setting.setPageInfo(this.pageSize.isSelected() ? 1 : 0);
+        this.setting.setRememberPageSize(this.pageSize.isSelected() ? 1 : 0);
         this.setting.setTabStrategy(this.tabStrategy.getStrategy());
         this.setting.setKeyLoadLimit(this.keyLoadLimit.getIntValue());
         this.setting.setTabLimit(this.tabLimit.getValue().intValue());
