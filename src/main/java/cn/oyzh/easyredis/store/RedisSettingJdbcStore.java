a89317aa92f087cf304675b6047679a5164dc363
oyzh <oyzh.1994@qq.com>
2024-11-29 16:25:19 +0800

    1. 新版本改造

---
+++ src/main/java/cn/oyzh/easyredis/store/RedisSettingJdbcStore.java
@@ -0,0 +1,55 @@
+package cn.oyzh.easyredis.store;
+
+import cn.oyzh.easyredis.domain.RedisSetting;
+import cn.oyzh.store.jdbc.JdbcStore;
+
+
+/**
+ * @author oyzh
+ * @since 2024/09/23
+ */
+public class RedisSettingJdbcStore extends JdbcStore<RedisSetting> {
+
+    /**
+     * 当前实例
+     */
+    public static final RedisSettingJdbcStore INSTANCE = new RedisSettingJdbcStore();
+
+    /**
+     * 当前设置
+     */
+    public static final RedisSetting SETTING = INSTANCE.load();
+
+    /**
+     * 数据id
+     */
+    private static final String DATA_UID = "DEFAULT";
+
+    public RedisSetting load() {
+        RedisSetting setting = super.selectOne(DATA_UID);
+        if (setting == null) {
+            setting = new RedisSetting();
+        }
+        return setting;
+    }
+
+    public boolean replace(RedisSetting model) {
+        if (model != null) {
+            if (super.exist(DATA_UID)) {
+                return this.update(model);
+            }
+            return this.insert(model);
+        }
+        return false;
+    }
+
+    @Override
+    protected RedisSetting newModel() {
+        return new RedisSetting();
+    }
+
+    @Override
+    protected Class<RedisSetting> modelClass() {
+        return RedisSetting.class;
+    }
+}
--- src/main/java/cn/oyzh/easyredis/store/RedisSettingStore.java
+++ src/main/java/cn/oyzh/easyredis/store/RedisSettingStore.java
@@ -1,59 +1,59 @@
-package cn.oyzh.easyredis.store;
-
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSONUtil;
-import cn.hutool.log.StaticLog;
-import cn.oyzh.easyredis.RedisConst;
-import cn.oyzh.easyredis.domain.RedisSetting;
-import cn.oyzh.fx.common.store.ObjectFileStore;
-import lombok.NonNull;
-
-
-/**
- * redis设置储存
- *
- * @author oyzh
- * @since 2026/06/26
- */
-//@Slf4j
-public class RedisSettingStore extends ObjectFileStore<RedisSetting> {
-
-    /**
-     * 当前实例
-     */
-    public static final RedisSettingStore INSTANCE = new RedisSettingStore();
-
-    /**
-     * 当前设置
-     */
-    public static final RedisSetting SETTING = INSTANCE.load();
-
-    {
-        this.filePath(RedisConst.STORE_PATH + "redis_setting.json");
-        JulLog.info("RedisSettingStore filePath:{} charset:{} init {}.", this.filePath(), this.charset(), super.init() ? "success" : "fail");
-    }
-
-    @Override
-    public synchronized RedisSetting load() {
-        RedisSetting setting = null;
-        String text = FileUtil.readString(this.storeFile(), this.charset());
-        if (StrUtil.isNotBlank(text)) {
-            try {
-                setting = JSONUtil.toBean(text, RedisSetting.class);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        if (setting == null) {
-            setting = new RedisSetting();
-        }
-        return setting;
-    }
-
-    @Override
-    public boolean update(@NonNull RedisSetting setting) {
-        return this.saveData(setting);
-    }
-
-}
+// package cn.oyzh.easyredis.store;
+//
+// import cn.hutool.core.io.FileUtil;
+// import cn.hutool.core.util.StrUtil;
+// import cn.hutool.json.JSONUtil;
+// import cn.hutool.log.StaticLog;
+// import cn.oyzh.easyredis.RedisConst;
+// import cn.oyzh.easyredis.domain.RedisSetting;
+// import cn.oyzh.fx.common.store.ObjectFileStore;
+// import lombok.NonNull;
+//
+//
+// /**
+//  * redis设置储存
+//  *
+//  * @author oyzh
+//  * @since 2026/06/26
+//  */
+// //@Slf4j
+// public class RedisSettingStore extends ObjectFileStore<RedisSetting> {
+//
+//     /**
+//      * 当前实例
+//      */
+//     public static final RedisSettingStore INSTANCE = new RedisSettingStore();
+//
+//     /**
+//      * 当前设置
+//      */
+//     public static final RedisSetting SETTING = INSTANCE.load();
+//
+//     {
+//         this.filePath(RedisConst.STORE_PATH + "redis_setting.json");
+//         JulLog.info("RedisSettingStore filePath:{} charset:{} init {}.", this.filePath(), this.charset(), super.init() ? "success" : "fail");
+//     }
+//
+//     @Override
+//     public synchronized RedisSetting load() {
+//         RedisSetting setting = null;
+//         String text = FileUtil.readString(this.storeFile(), this.charset());
+//         if (StrUtil.isNotBlank(text)) {
+//             try {
+//                 setting = JSONUtil.toBean(text, RedisSetting.class);
+//             } catch (Exception ex) {
+//                 ex.printStackTrace();
+//             }
+//         }
+//         if (setting == null) {
+//             setting = new RedisSetting();
+//         }
+//         return setting;
+//     }
+//
+//     @Override
+//     public boolean update(@NonNull RedisSetting setting) {
+//         return this.saveData(setting);
+//     }
+//
+// }
