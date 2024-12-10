0953bd4a1cdb855ba404577466452ce8c9c5963e
oyzh <oyzh.1994@qq.com>
2024-12-10 16:14:39 +0800

    1. redis新版导出初步开发

---
+++ src/main/java/cn/oyzh/easyredis/controller/data/RedisDataExportController.java
@@ -0,0 +1,406 @@
+package cn.oyzh.easyredis.controller.data;
+
+import cn.oyzh.common.thread.DownLatch;
+import cn.oyzh.common.thread.ThreadUtil;
+import cn.oyzh.common.util.FileNameUtil;
+import cn.oyzh.common.util.FileUtil;
+import cn.oyzh.easyredis.RedisConst;
+import cn.oyzh.easyredis.domain.RedisConnect;
+import cn.oyzh.easyredis.handler.RedisDataExportHandler;
+import cn.oyzh.easyredis.redis.RedisClient;
+import cn.oyzh.easyredis.redis.RedisClientUtil;
+import cn.oyzh.easyredis.store.RedisFilterJdbcStore;
+import cn.oyzh.fx.gui.combobox.CharsetComboBox;
+import cn.oyzh.fx.gui.text.area.MsgTextArea;
+import cn.oyzh.fx.plus.controller.StageController;
+import cn.oyzh.fx.plus.controls.box.FlexVBox;
+import cn.oyzh.fx.plus.controls.button.FXButton;
+import cn.oyzh.fx.plus.controls.button.FXCheckBox;
+import cn.oyzh.fx.plus.controls.button.FlexButton;
+import cn.oyzh.fx.plus.controls.label.FXLabel;
+import cn.oyzh.fx.plus.controls.text.FXText;
+import cn.oyzh.fx.plus.controls.toggle.FXToggleGroup;
+import cn.oyzh.fx.plus.file.FileChooserHelper;
+import cn.oyzh.fx.plus.file.FileExtensionFilter;
+import cn.oyzh.fx.plus.information.MessageBox;
+import cn.oyzh.fx.plus.node.NodeGroupUtil;
+import cn.oyzh.fx.plus.util.Counter;
+import cn.oyzh.fx.plus.util.FXUtil;
+import cn.oyzh.fx.plus.window.StageAttribute;
+import cn.oyzh.i18n.I18nHelper;
+import javafx.fxml.FXML;
+import javafx.stage.Modality;
+import javafx.stage.WindowEvent;
+
+import java.io.File;
+
+
+/**
+ * zk数据导出业务
+ *
+ * @author oyzh
+ * @since 2024/11/26
+ */
+@StageAttribute(
+        iconUrls = RedisConst.ICON_PATH,
+        modality = Modality.APPLICATION_MODAL,
+        value = RedisConst.FXML_BASE_PATH + "data/redisDataExport.fxml"
+)
+public class RedisDataExportController extends StageController {
+
+    /**
+     * 第一步
+     */
+    @FXML
+    private FlexVBox step1;
+
+    /**
+     * 第二步
+     */
+    @FXML
+    private FlexVBox step2;
+
+    /**
+     * 第三步
+     */
+    @FXML
+    private FlexVBox step3;
+
+    /**
+     * 第四步
+     */
+    @FXML
+    private FlexVBox step4;
+
+    /**
+     * 导出文件
+     */
+    private File exportFile;
+
+    /**
+     * 文件格式
+     */
+    @FXML
+    private FXToggleGroup format;
+
+    /**
+     * 前缀
+     */
+    @FXML
+    private FXToggleGroup prefix;
+
+    /**
+     * 文件名
+     */
+    @FXML
+    private FXText fileName;
+
+    /**
+     * 节点路径
+     */
+    @FXML
+    private FXText nodePath;
+
+    /**
+     * 字符集
+     */
+    @FXML
+    private CharsetComboBox charset;
+
+    /**
+     * 选择文件
+     */
+    @FXML
+    private FXButton selectFile;
+
+    /**
+     * 适用过滤配置
+     */
+    @FXML
+    private FXCheckBox applyFilter;
+
+    /**
+     * 结束导出按钮
+     */
+    @FXML
+    private FlexButton stopExportBtn;
+
+    /**
+     * 导出状态
+     */
+    @FXML
+    private FXLabel exportStatus;
+
+    /**
+     * 导出消息
+     */
+    @FXML
+    private MsgTextArea exportMsg;
+
+    /*
+     * 导出路径
+     */
+    private String exportPath;
+
+    /**
+     * 当前zk对象
+     */
+    private RedisConnect connect;
+
+    /**
+     * 当前zk客户端
+     */
+    private RedisClient client;
+
+    /**
+     * 导出操作任务
+     */
+    private Thread execTask;
+
+    /**
+     * 计数器
+     */
+    private final Counter counter = new Counter();
+
+    /**
+     * 过滤配置储存
+     */
+    private final RedisFilterJdbcStore filterStore = RedisFilterJdbcStore.INSTANCE;
+
+    /**
+     * 导出处理器
+     */
+    private RedisDataExportHandler exportHandler;
+
+    /**
+     * 执行导出
+     */
+    @FXML
+    private void doExport() {
+        // 重置参数
+        // 清理信息
+        this.counter.reset();
+        this.exportMsg.clear();
+        this.exportStatus.clear();
+        NodeGroupUtil.disable(this.stage, "exec");
+        this.stage.appendTitle("===" + I18nHelper.exportProcessing() + "===");
+        // 生成迁移处理器
+        if (this.exportHandler == null) {
+            this.exportHandler = new RedisDataExportHandler();
+            this.exportHandler
+                    .messageHandler(str -> this.exportMsg.appendLine(str))
+                    .processedHandler(count -> {
+                        if (count == 0) {
+                            this.counter.updateIgnore();
+                        } else if (count < 0) {
+                            this.counter.incrFail(count);
+                        } else {
+                            this.counter.incrSuccess(count);
+                        }
+                        this.updateStatus(I18nHelper.exportInProgress());
+                    });
+        } else {
+            this.exportHandler.interrupt(false);
+        }
+        String fileType = this.format.selectedUserData();
+        // 文件类型
+        this.exportHandler.fileType(fileType);
+        // 客户端
+        this.exportHandler.client(this.client);
+        // 节点路径
+        this.exportHandler.nodePath(this.exportPath);
+        // 导出文件
+        this.exportHandler.filePath(this.exportFile.getPath());
+        // 字符集
+        this.exportHandler.charset(this.charset.getCharsetName());
+        // 适用过滤
+        if (this.applyFilter.isSelected()) {
+            this.exportHandler.filters(this.filterStore.loadEnable());
+        } else {
+            this.exportHandler.filters(null);
+        }
+        // 前缀
+        if (FileNameUtil.isTxtType(fileType)) {
+            this.exportHandler.prefix(this.prefix.selectedUserData());
+        }
+        // 执行导出
+        this.execTask = ThreadUtil.start(() -> {
+            try {
+                this.stopExportBtn.enable();
+                // 更新状态
+                this.updateStatus(I18nHelper.exportStarting());
+                // 执行导出
+                this.exportHandler.doExport();
+                // 更新状态
+                this.updateStatus(I18nHelper.exportFinished());
+            } catch (Exception e) {
+                if (e.getClass().isAssignableFrom(InterruptedException.class)) {
+                    this.updateStatus(I18nHelper.operationCancel());
+                    MessageBox.warn(I18nHelper.operationCancel());
+                } else {
+                    e.printStackTrace();
+                    this.updateStatus(I18nHelper.operationFail());
+                    MessageBox.warn(I18nHelper.operationFail());
+                }
+            } finally {
+                NodeGroupUtil.enable(this.stage, "exec");
+                this.stopExportBtn.disable();
+                this.stage.restoreTitle();
+            }
+        });
+    }
+
+    /**
+     * 结束导出
+     */
+    @FXML
+    private void stopExport() {
+        ThreadUtil.interrupt(this.execTask);
+        this.execTask = null;
+        if (this.exportHandler != null) {
+            this.exportHandler.interrupt();
+        }
+    }
+
+    @Override
+    protected void bindListeners() {
+        super.bindListeners();
+        this.stage.hideOnEscape();
+        // 格式选择监听
+        this.format.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            this.exportFile = null;
+            this.fileName.clear();
+        });
+    }
+
+    // @Override
+    // public void onStageInitialize(StageAdapter stage) {
+    //     super.onStageInitialize(stage);
+    //     this.step1.managedBindVisible();
+    //     this.step2.managedBindVisible();
+    //     this.step3.managedBindVisible();
+    //     this.step4.managedBindVisible();
+    // }
+
+    @Override
+    public void onWindowHidden(WindowEvent event) {
+        super.onWindowHidden(event);
+        this.stopExport();
+    }
+
+    /**
+     * 更新状态
+     *
+     * @param extraMsg 额外信息
+     */
+    private void updateStatus(String extraMsg) {
+        if (extraMsg != null) {
+            this.counter.setExtraMsg(extraMsg);
+        }
+        FXUtil.runLater(() -> this.exportStatus.setText(this.counter.unknownFormat()));
+    }
+
+    @Override
+    public String getViewTitle() {
+        return I18nHelper.exportTitle();
+    }
+
+    @FXML
+    private void showStep1() {
+        this.step2.disappear();
+        this.step3.disappear();
+        this.step4.disappear();
+        this.step1.display();
+    }
+
+    @FXML
+    private void showStep2() {
+        this.step1.disappear();
+        this.step3.disappear();
+        this.step4.disappear();
+        String fileType = this.format.selectedUserData();
+        if (FileNameUtil.isTxtType(fileType)) {
+            NodeGroupUtil.enable(this.stage, "txt");
+        } else {
+            NodeGroupUtil.disable(this.stage, "txt");
+        }
+        this.step2.display();
+    }
+
+    @FXML
+    private void showStep3() {
+        if (this.exportFile == null) {
+            this.selectFile.requestFocus();
+            MessageBox.warn(I18nHelper.pleaseSelectFile());
+            return;
+        }
+        this.step1.disappear();
+        this.step2.disappear();
+        this.step4.disappear();
+        this.step3.display();
+    }
+
+    @FXML
+    private void showStep4() {
+        try {
+            // 检查客户端
+            if (this.client == null || this.client.isClosed()) {
+                this.getStage().appendTitle("===" + I18nHelper.connectIng() + "===");
+                this.getStage().disable();
+                DownLatch latch = DownLatch.of();
+                ThreadUtil.start(() -> {
+                    try {
+                        this.client = RedisClientUtil.newClient(this.connect);
+                        this.client.start(2500);
+                    } finally {
+                        latch.countDown();
+                    }
+                });
+                if (!latch.await(3000) || !this.client.isConnected()) {
+                    this.client.close();
+                    this.client = null;
+                    MessageBox.warn(I18nHelper.connectInitFail());
+                    return;
+                }
+            }
+
+            this.step1.disappear();
+            this.step2.disappear();
+            this.step3.disappear();
+            this.step4.display();
+        } finally {
+            this.getStage().restoreTitle();
+            this.getStage().enable();
+        }
+    }
+
+    /**
+     * 选择文件
+     */
+    @FXML
+    private void selectFile() {
+        String fileType = this.format.selectedUserData();
+        FileExtensionFilter filter = FileChooserHelper.extensionFilter(fileType);
+        String fileName = "Zookeeper-" + this.connect.getName() + "-" + I18nHelper.exportData() + "." + fileType;
+        this.exportFile = FileChooserHelper.save(fileName, fileName, filter);
+        if (this.exportFile != null) {
+            // 删除文件
+            if (this.exportFile.exists()) {
+                FileUtil.del(this.exportFile);
+            }
+            this.fileName.setText(this.exportFile.getPath());
+        } else {
+            this.fileName.clear();
+        }
+    }
+
+    @Override
+    public void onStageShown(WindowEvent event) {
+        super.onStageShown(event);
+        this.connect = this.getWindowProp("connect");
+        this.exportPath = this.getWindowProp("nodePath");
+        this.nodePath.setText(this.exportPath);
+    }
+
+
+}
---
+++ src/main/java/cn/oyzh/easyredis/handler/RedisDataExportHandler.java
@@ -0,0 +1,173 @@
+package cn.oyzh.easyredis.handler;
+
+import cn.oyzh.common.log.JulLog;
+import cn.oyzh.common.util.StringUtil;
+import cn.oyzh.easyredis.domain.RedisFilter;
+import cn.oyzh.easyredis.redis.RedisClient;
+import cn.oyzh.easyredis.util.RedisKeyUtil;
+import cn.oyzh.store.file.FileColumns;
+import cn.oyzh.store.file.FileHelper;
+import cn.oyzh.store.file.FileRecord;
+import cn.oyzh.store.file.FileWriteConfig;
+import cn.oyzh.store.file.TypeFileWriter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+/**
+ * @author oyzh
+ * @since 2024/11/26
+ */
+@Setter
+@Accessors(fluent = true, chain = false)
+public class RedisDataExportHandler extends DataHandler {
+
+    /**
+     * 文件格式
+     */
+    private String fileType;
+
+    /**
+     * 客户端
+     */
+    private RedisClient client;
+
+    /**
+     * 节点路径
+     */
+    private String nodePath;
+
+    /**
+     * 过滤内容列表
+     */
+    private List<RedisFilter> filters;
+
+    /**
+     * 批量处理大小
+     */
+    private int batchSize = 20;
+
+    /**
+     * 导出配置
+     */
+    private FileWriteConfig config = new FileWriteConfig();
+
+    /**
+     * 执行导出
+     *
+     * @throws Exception 异常
+     */
+    public void doExport() throws Exception {
+        this.message("Export Starting");
+        FileColumns columns = new FileColumns();
+        columns.addColumn("path");
+        columns.addColumn("data");
+        // 获取写入器
+        TypeFileWriter writer = FileHelper.initWriter(this.fileType, this.config, columns);
+        if (writer != null) {
+            // 批量记录
+            List<FileRecord> batchList = new ArrayList<>(this.batchSize);
+            // 批量写入函数
+            Runnable writeBatch = () -> {
+                try {
+                    writer.writeRecords(batchList);
+                    for (FileRecord record : batchList) {
+                        this.message("export node[" + record.get(0) + "] success");
+                    }
+                    this.processedIncr(batchList.size());
+                    batchList.clear();
+                } catch (Exception ex) {
+                    this.message("write data failed");
+                    this.processedDecr();
+                }
+            };
+            try {
+                // 写入头
+                writer.writeHeader();
+                // 节点过滤
+                Predicate<String> filter = path -> {
+                    if (RedisKeyUtil.isFiltered(path, this.filters)) {
+                        this.message("node[" + path + "] is filtered, skip it");
+                        this.processedSkip();
+                        return false;
+                    }
+                    return true;
+                };
+
+                // 获取节点成功
+                BiConsumer<String, byte[]> success = (path, bytes) -> {
+                    try {
+                        this.checkInterrupt();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    // 记录
+                    FileRecord record = new FileRecord();
+                    record.put(0, path);
+                    record.put(1, new String(bytes, StandardCharsets.UTF_8));
+                    // 添加到集合
+                    batchList.add(record);
+
+                    // 批量写入
+                    if (batchList.size() >= this.batchSize) {
+                        writeBatch.run();
+                    }
+                };
+                // 获取节点失败
+                BiConsumer<String, Exception> error = (path, ex) -> {
+                    if (ex instanceof RuntimeException) {
+                        ex = (Exception) ex.getCause();
+                    } else {
+                        ex.printStackTrace();
+                    }
+                    // 针对中断异常不处理
+                    if (ex instanceof InterruptedException) {
+                        return;
+                    }
+                    this.message("export node[" + path + "] failed");
+                    this.processedDecr();
+                };
+            } finally {
+                // 写入尾
+                writeBatch.run();
+                writer.writeTrial();
+                writer.close();
+                this.message("Exported To -> " + this.config.filePath());
+            }
+        } else {
+            JulLog.error("未找到可用的写入器，文件类型:{}", this.fileType);
+        }
+        this.message("Export Finished");
+    }
+
+    public void prefix(String prefix) {
+        if (prefix.isBlank()) {
+            this.config.prefix(null);
+        } else {
+
+            this.config.prefix(prefix + " ");
+        }
+    }
+
+    public void charset(String charset) {
+        if (StringUtil.isBlank(charset)) {
+            this.config.charset(StandardCharsets.UTF_8.name());
+        } else {
+            this.config.charset(charset);
+        }
+    }
+
+    public void filePath(String filePath) {
+        this.config.filePath(filePath);
+    }
+
+    public void txtIdentifier(Character txtIdentifier) {
+        this.config.txtIdentifier(txtIdentifier);
+    }
+}
+
