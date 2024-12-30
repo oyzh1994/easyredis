dee0eb196266a14841d2bdc09cef0fa53343217e
oyzh <oyzh.1994@qq.com>
2024-12-30 16:34:24 +0800

    1. 部分无效文件注释

--- src/main/java/cn/oyzh/easyredis/terminal/RedisTerminalHistoryStore.java
+++ src/main/java/cn/oyzh/easyredis/terminal/RedisTerminalHistoryStore.java
@@ -1,27 +1,27 @@
-package cn.oyzh.easyredis.terminal;
-
-import cn.oyzh.common.log.JulLog;
-import cn.oyzh.easyredis.RedisConst;
-import cn.oyzh.fx.terminal.histroy.TerminalHistoryStore;
-
-/**
- * redis终端命令历史
- *
- * @author oyzh
- * @since 2023/7/21
- */
-//@Slf4j
-@Deprecated
-public class RedisTerminalHistoryStore extends TerminalHistoryStore {
-
-    /**
-     * 当前实例
-     */
-    public static final RedisTerminalHistoryStore INSTANCE = new RedisTerminalHistoryStore();
-
-    {
-        this.filePath(RedisConst.STORE_PATH + "redis_shell_history.json");
-        JulLog.info("RedisShellHistoryStore filePath:{} charset:{} init {}.", this.filePath(), this.charset(), super.init() ? "success" : "fail");
-    }
-
-}
+// package cn.oyzh.easyredis.terminal;
+//
+// import cn.oyzh.common.log.JulLog;
+// import cn.oyzh.easyredis.RedisConst;
+// import cn.oyzh.fx.terminal.histroy.TerminalHistoryStore;
+//
+// /**
+//  * redis终端命令历史
+//  *
+//  * @author oyzh
+//  * @since 2023/7/21
+//  */
+// //@Slf4j
+// @Deprecated
+// public class RedisTerminalHistoryStore extends TerminalHistoryStore {
+//
+//     /**
+//      * 当前实例
+//      */
+//     public static final RedisTerminalHistoryStore INSTANCE = new RedisTerminalHistoryStore();
+//
+//     {
+//         this.filePath(RedisConst.STORE_PATH + "redis_shell_history.json");
+//         JulLog.info("RedisShellHistoryStore filePath:{} charset:{} init {}.", this.filePath(), this.charset(), super.init() ? "success" : "fail");
+//     }
+//
+// }
