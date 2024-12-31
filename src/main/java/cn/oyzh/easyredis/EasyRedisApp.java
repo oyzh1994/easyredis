829f35d1c39f0754035c291ee43f6e6ee3981f02
oyzh <oyzh.1994@qq.com>
2024-12-31 10:42:34 +0800

    1. 终端命令注册

--- src/main/java/cn/oyzh/easyredis/EasyRedisApp.java
+++ src/main/java/cn/oyzh/easyredis/EasyRedisApp.java
@@ -10,10 +10,12 @@ import cn.oyzh.easyredis.domain.RedisSetting;
 import cn.oyzh.easyredis.exception.RedisExceptionParser;
 import cn.oyzh.easyredis.store.RedisSettingStore;
 import cn.oyzh.easyredis.store.RedisStoreUtil;
+import cn.oyzh.easyredis.terminal.RedisTerminalManager;
 import cn.oyzh.event.EventFactory;
 import cn.oyzh.fx.gui.tray.DesktopTrayItem;
 import cn.oyzh.fx.gui.tray.QuitTrayItem;
 import cn.oyzh.fx.gui.tray.SettingTrayItem;
+import cn.oyzh.fx.plus.FXConst;
 import cn.oyzh.fx.plus.event.FxEventBus;
 import cn.oyzh.fx.plus.event.FxEventConfig;
 import cn.oyzh.fx.plus.ext.FXApplication;
@@ -25,7 +27,6 @@ import cn.oyzh.fx.plus.tray.TrayManager;
 import cn.oyzh.fx.plus.util.FXUtil;
 import cn.oyzh.fx.plus.window.StageAdapter;
 import cn.oyzh.fx.plus.window.StageManager;
-import cn.oyzh.fx.terminal.TerminalConst;
 import cn.oyzh.i18n.I18nManager;
 import javafx.stage.Stage;
 
@@ -46,24 +47,38 @@ public class EasyRedisApp extends FXApplication {
     private static final Project PROJECT = Project.load();
 
     public static void main(String[] args) {
-        SysConst.storeDir(RedisConst.STORE_PATH);
-        SysConst.cacheDir(RedisConst.CACHE_PATH);
-        SysConst.projectName(PROJECT.getName());
-        EventFactory.registerEventBus(FxEventBus.class);
-        EventFactory.syncEventConfig(FxEventConfig.SYNC);
-        EventFactory.asyncEventConfig(FxEventConfig.ASYNC);
-        EventFactory.defaultEventConfig(FxEventConfig.DEFAULT);
-        TerminalConst.scanBase("cn.oyzh.easyredis.terminal");
-        JulLog.info("项目启动中...");
-        launch(EasyRedisApp.class, args);
+        try {
+            SysConst.projectName(PROJECT.getName());
+            JulLog.info("项目启动中...");
+            // 储存初始化
+            RedisStoreUtil.init();
+            SysConst.storeDir(RedisConst.STORE_PATH);
+            SysConst.cacheDir(RedisConst.CACHE_PATH);
+            FXConst.appIcon(RedisConst.ICON_PATH);
+            EventFactory.registerEventBus(FxEventBus.class);
+            EventFactory.syncEventConfig(FxEventConfig.SYNC);
+            EventFactory.asyncEventConfig(FxEventConfig.ASYNC);
+            EventFactory.defaultEventConfig(FxEventConfig.DEFAULT);
+            // TerminalConst.scanBase("cn.oyzh.easyredis.terminal");
+            // 初始化时区处理器
+            // System.setProperty("java.time.zone.DefaultZoneRulesProvider", LocalZoneRulesProvider.class.getName());
+            launch(EasyRedisApp.class, args);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            JulLog.warn("main error", ex);
+        }
     }
 
     @Override
     public void init() {
         try {
-            // 储存初始化
-            RedisStoreUtil.init();
-            // 设置对象
+            // fx程序实例
+            FXConst.INSTANCE = this;
+            // 日志开始
+            JulLog.info("{} init start.", SysConst.projectName());
+            // 禁用fx的css日志
+            FXUtil.disableCSSLogger();
+            // 配置对象
             RedisSetting setting = RedisSettingStore.SETTING;
             // 应用区域
             I18nManager.apply(setting.getLocale());
@@ -79,44 +94,47 @@ public class EasyRedisApp extends FXApplication {
             super.init();
         } catch (Exception ex) {
             ex.printStackTrace();
+            JulLog.warn("main error", ex);
         }
     }
 
     @Override
     public void start(Stage primaryStage) {
-        super.start(primaryStage);
-        // 显示迁移弹窗
-        if (RedisStoreUtil.checkOlder()) {
-            FXUtil.runWait(() -> StageManager.showStage(RedisMigrationTipsController.class), 1000);
+        try {
+            super.start(primaryStage);
+            // 注册命令
+            RedisTerminalManager.registerHandlers();
+            // 显示迁移弹窗
+            if (RedisStoreUtil.checkOlder()) {
+                FXUtil.runWait(() -> StageManager.showStage(RedisMigrationTipsController.class), 1000);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            JulLog.warn("start error", ex);
         }
     }
 
     @Override
     protected void showMainView() {
-        // 显示主页面
-        StageManager.showStage(MainController.class);
+        try {
+            // 显示主页面
+            StageManager.showStage(MainController.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            JulLog.warn("showMainView error", ex);
+        }
     }
-
-    // @Override
-    // protected String appIcon() {
-    //     return RedisConst.ICON_PATH;
-    // }
-    //
-    // @Override
-    // protected String appName() {
-    //     return PROJECT.getName();
-    // }
 
     @Override
     protected void initSystemTray() {
-        if (!TrayManager.supported()) {
-            JulLog.warn("tray is not supported.");
-            return;
-        }
-        if (TrayManager.exist()) {
-            return;
-        }
         try {
+            if (!TrayManager.supported()) {
+                JulLog.warn("tray is not supported.");
+                return;
+            }
+            if (TrayManager.exist()) {
+                return;
+            }
             // 初始化
             TrayManager.init(RedisConst.TRAY_ICON_PATH);
             // 设置标题
