5e7242b62e8fc519edd2592a221964179d4addd1
oyzh <oyzh.1994@q.qcom>
2025-01-03 15:15:21 +0800

    1. 终端支持字体配置

--- src/main/java/cn/oyzh/easyredis/terminal/RedisTerminalTextTextArea.java
+++ src/main/java/cn/oyzh/easyredis/terminal/RedisTerminalTextTextArea.java
@@ -3,10 +3,11 @@ package cn.oyzh.easyredis.terminal;
 import cn.oyzh.common.log.JulLog;
 import cn.oyzh.common.thread.ExecutorUtil;
 import cn.oyzh.easyredis.domain.RedisConnect;
+import cn.oyzh.easyredis.domain.RedisSetting;
 import cn.oyzh.easyredis.dto.RedisConnectInfo;
-import cn.oyzh.easyredis.exception.RedisExceptionParser;
 import cn.oyzh.easyredis.redis.RedisClient;
 import cn.oyzh.easyredis.redis.RedisConnState;
+import cn.oyzh.easyredis.store.RedisSettingStore;
 import cn.oyzh.easyredis.util.RedisConnectUtil;
 import cn.oyzh.fx.plus.i18n.I18nResourceBundle;
 import cn.oyzh.fx.terminal.TerminalTextArea;
@@ -15,7 +16,6 @@ import javafx.beans.value.ChangeListener;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.experimental.Accessors;
-import redis.clients.jedis.exceptions.JedisConnectionException;
 
 /**
  * redis终端文本域
@@ -31,6 +31,17 @@ public class RedisTerminalTextTextArea extends TerminalTextArea {
         this.mouseHandler(RedisTerminalMouseHandler.INSTANCE);
         this.historyHandler(RedisTerminalHistoryHandler.INSTANCE);
         this.completeHandler(RedisTerminalCompleteHandler.INSTANCE);
+    }
+
+    @Override
+    protected void initFont() {
+        // 禁用字体管理
+        super.disableFont();
+        // 初始化字体
+        RedisSetting setting= RedisSettingStore.SETTING;
+        this.setFontSize(setting.getTerminalFontSize());
+        this.setFontFamily(setting.getTerminalFontFamily());
+        this.setFontWeight2(setting.getTerminalFontWeight());
     }
 
     /**
