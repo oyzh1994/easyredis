046d3d356da4073c18209c41a5806694f0fc3c61
oyzh <oyzh.1994@q.qcom>
2025-02-12 16:54:11 +0800

    1. 细节调整

--- src/main/java/cn/oyzh/easyredis/RedisConst.java
+++ src/main/java/cn/oyzh/easyredis/RedisConst.java
@@ -13,11 +13,6 @@ import java.io.File;
 @UtilityClass
 public class RedisConst {
 
-    // /**
-    //  * fxml基础地址
-    //  */
-    // public final static String FXML_BASE_PATH = "/fxml/";
-
     /**
      * 数据保存路径
      */
