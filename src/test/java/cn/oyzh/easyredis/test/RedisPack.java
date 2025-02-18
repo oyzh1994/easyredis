608e7bcace80adb72e6e40a2c59304198e56e28f
oyzh <oyzh.1994@q.qcom>
2025-02-18 14:39:05 +0800

    1. md相关更新 2. 新增pkg打包，macos推荐 3. 其他细节处理

--- src/test/java/cn/oyzh/easyredis/test/RedisPack.java
+++ src/test/java/cn/oyzh/easyredis/test/RedisPack.java
@@ -70,10 +70,54 @@ public class RedisPack {
         packer.pack(win_pack_config, properties);
     }
 
+//    @Test
+//    public void easyredis_win_amd64_pack() throws Exception {
+//        String packagePath = this.getPackagePath();
+//        String win_pack_config = packagePath + "win_amd64_pack_config.json";
+//        String getProjectPath = this.getProjectPath();
+//        Map<String, Object> properties = new HashMap<>();
+//        properties.put("projectPath", getProjectPath);
+//
+//        Packer packer = new Packer();
+//        packer.registerProjectHandler();
+//        packer.registerJdepsHandler();
+//        packer.pack(win_pack_config, properties);
+//    }
+
+//    @Test
+//    public void easyredis_linux_amd64_pack() throws Exception {
+//        String packagePath = this.getPackagePath();
+//        String linux_pack_config = packagePath + "linux_amd64_pack_config.json";
+//
+//        Packer packer = new Packer();
+//        packer.registerProjectHandler();
+//        packer.pack(linux_pack_config);
+//    }
+//
+//    @Test
+//    public void easyredis_linux_arm64_pack() throws Exception {
+//        String packagePath = this.getPackagePath();
+//        String linux_pack_config = packagePath + "linux_arm64_pack_config.json";
+//
+//        Packer packer = new Packer();
+//        packer.registerProjectHandler();
+//        packer.pack(linux_pack_config);
+//    }
+//
+//    @Test
+//    public void easyredis_macos_amd64_pack() throws Exception {
+//        String packagePath = this.getPackagePath();
+//        String macos_pack_config = packagePath + "macos_amd64_pack_config.json";
+//
+//        Packer packer = new Packer();
+//        packer.registerProjectHandler();
+//        packer.pack(macos_pack_config);
+//    }
+
     @Test
-    public void easyredis_win_amd64_pack() throws Exception {
+    public void easyredis_macos_arm64_dmg() throws Exception {
         String packagePath = this.getPackagePath();
-        String win_pack_config = packagePath + "win_amd64_pack_config.json";
+        String macos_arm64_pack_config = packagePath + "/macos/macos_arm64_dmg.json";
         String getProjectPath = this.getProjectPath();
         Map<String, Object> properties = new HashMap<>();
         properties.put("projectPath", getProjectPath);
@@ -81,43 +125,13 @@ public class RedisPack {
         Packer packer = new Packer();
         packer.registerProjectHandler();
         packer.registerJdepsHandler();
-        packer.pack(win_pack_config, properties);
+        packer.pack(macos_arm64_pack_config, properties);
     }
 
     @Test
-    public void easyredis_linux_amd64_pack() throws Exception {
+    public void easyredis_macos_arm64_pkg() throws Exception {
         String packagePath = this.getPackagePath();
-        String linux_pack_config = packagePath + "linux_amd64_pack_config.json";
-
-        Packer packer = new Packer();
-        packer.registerProjectHandler();
-        packer.pack(linux_pack_config);
-    }
-
-    @Test
-    public void easyredis_linux_arm64_pack() throws Exception {
-        String packagePath = this.getPackagePath();
-        String linux_pack_config = packagePath + "linux_arm64_pack_config.json";
-
-        Packer packer = new Packer();
-        packer.registerProjectHandler();
-        packer.pack(linux_pack_config);
-    }
-
-    @Test
-    public void easyredis_macos_amd64_pack() throws Exception {
-        String packagePath = this.getPackagePath();
-        String macos_pack_config = packagePath + "macos_amd64_pack_config.json";
-
-        Packer packer = new Packer();
-        packer.registerProjectHandler();
-        packer.pack(macos_pack_config);
-    }
-
-    @Test
-    public void easyredis_macos_arm64_dmg() throws Exception {
-        String packagePath = this.getPackagePath();
-        String macos_arm64_pack_config = packagePath + "/macos/macos_arm64_dmg.json";
+        String macos_arm64_pack_config = packagePath + "/macos/macos_arm64_pkg.json";
         String getProjectPath = this.getProjectPath();
         Map<String, Object> properties = new HashMap<>();
         properties.put("projectPath", getProjectPath);
@@ -142,26 +156,26 @@ public class RedisPack {
         packer.pack(macos_arm64_pack_config, properties);
     }
 
-    @Test
-    public void easyredis_all_pack() throws Exception {
-        String packagePath = this.getPackagePath();
-        String win_amd64_pack_config = packagePath + "win_amd64_pack_config.json";
-        String linux_amd64_pack_config = packagePath + "linux_amd64_pack_config.json";
-        String linux_arm64_pack_config = packagePath + "linux_arm64_pack_config.json";
-        String macos_amd64_pack_config = packagePath + "macos_amd64_pack_config.json";
-        String macos_arm64_pack_config = packagePath + "macos_arm64_pack_config.json";
-
-        String baseDir = "D:\\Workspaces\\OYZH\\fx-base\\";
-        String projectDir = "D:\\Workspaces\\OYZH\\easyredis\\";
-
-        Packer packer = new Packer();
-        packer.registerProjectHandler();
-        packer.registerMvnHandler(projectDir, List.of(baseDir));
-        packer.pack(win_amd64_pack_config);
-        packer.pack(linux_amd64_pack_config);
-        packer.pack(linux_arm64_pack_config);
-        packer.pack(macos_amd64_pack_config);
-        packer.pack(macos_arm64_pack_config);
-    }
+//    @Test
+//    public void easyredis_all_pack() throws Exception {
+//        String packagePath = this.getPackagePath();
+//        String win_amd64_pack_config = packagePath + "win_amd64_pack_config.json";
+//        String linux_amd64_pack_config = packagePath + "linux_amd64_pack_config.json";
+//        String linux_arm64_pack_config = packagePath + "linux_arm64_pack_config.json";
+//        String macos_amd64_pack_config = packagePath + "macos_amd64_pack_config.json";
+//        String macos_arm64_pack_config = packagePath + "macos_arm64_pack_config.json";
+//
+//        String baseDir = "D:\\Workspaces\\OYZH\\fx-base\\";
+//        String projectDir = "D:\\Workspaces\\OYZH\\easyredis\\";
+//
+//        Packer packer = new Packer();
+//        packer.registerProjectHandler();
+//        packer.registerMvnHandler(projectDir, List.of(baseDir));
+//        packer.pack(win_amd64_pack_config);
+//        packer.pack(linux_amd64_pack_config);
+//        packer.pack(linux_arm64_pack_config);
+//        packer.pack(macos_amd64_pack_config);
+//        packer.pack(macos_arm64_pack_config);
+//    }
 
 }
