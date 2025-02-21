461f23f81b087fe3ad4dfa26ddd1e9b2f6bebb95
oyzh <oyzh.1994@q.qcom>
2025-02-21 15:51:50 +0800

    1. 页面显示事件驱动改造

---
+++ src/main/java/cn/oyzh/easyredis/event/window/RedisShowAddKeyEvent.java
@@ -0,0 +1,12 @@
+package cn.oyzh.easyredis.event.window;
+
+import cn.oyzh.easyredis.domain.RedisGroup;
+import cn.oyzh.easyredis.trees.connect.RedisDatabaseTreeItem;
+import cn.oyzh.event.Event;
+
+/**
+ * @author oyzh
+ * @since 2025-02-20
+ */
+public class RedisShowAddKeyEvent extends Event<RedisDatabaseTreeItem> {
+}
