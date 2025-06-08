36197c0fb1d64d130d721fbe4d06895917bd1673
oyzh <oyzh.1994@qq.com>
2025-06-08 17:49:05 +0800

    1. 字体设置改造，优化体验

--- src/main/java/cn/oyzh/easyredis/controller/SettingController2.java
+++ src/main/java/cn/oyzh/easyredis/controller/SettingController2.java
@@ -5,6 +5,7 @@ import cn.oyzh.common.util.StringUtil;
 import cn.oyzh.easyredis.domain.RedisSetting;
 import cn.oyzh.easyredis.store.RedisSettingStore;
 import cn.oyzh.easyredis.util.RedisProcessUtil;
+import cn.oyzh.fx.gui.font.FontFamilyTextField;
 import cn.oyzh.fx.gui.setting.SettingLeftItem;
 import cn.oyzh.fx.gui.setting.SettingLeftTreeView;
 import cn.oyzh.fx.gui.setting.SettingMainPane;
@@ -18,7 +19,6 @@ import cn.oyzh.fx.plus.controls.picker.FXColorPicker;
 import cn.oyzh.fx.plus.controls.text.FXSlider;
 import cn.oyzh.fx.plus.controls.toggle.FXToggleGroup;
 import cn.oyzh.fx.plus.domain.AppSetting;
-import cn.oyzh.fx.plus.font.FontFamilyComboBox;
 import cn.oyzh.fx.plus.font.FontManager;
 import cn.oyzh.fx.plus.font.FontSizeComboBox;
 import cn.oyzh.fx.plus.font.FontWeightComboBox;
@@ -162,7 +162,7 @@ public class SettingController2 extends StageController {
      * 字体名称
      */
     @FXML
-    private FontFamilyComboBox fontFamily;
+    private FontFamilyTextField fontFamily;
 
     /**
      * 编辑器字体大小
@@ -180,7 +180,7 @@ public class SettingController2 extends StageController {
      * 编辑器字体名称
      */
     @FXML
-    private FontFamilyComboBox editorFontFamily;
+    private FontFamilyTextField editorFontFamily;
 
     /**
      * 终端字体大小
@@ -198,7 +198,7 @@ public class SettingController2 extends StageController {
      * 终端字体名称
      */
     @FXML
-    private FontFamilyComboBox terminalFontFamily;
+    private FontFamilyTextField terminalFontFamily;
 
     /**
      * 查询字体大小
@@ -216,7 +216,7 @@ public class SettingController2 extends StageController {
      * 查询字体名称
      */
     @FXML
-    private FontFamilyComboBox queryFontFamily;
+    private FontFamilyTextField queryFontFamily;
 
     /**
      * 区域
