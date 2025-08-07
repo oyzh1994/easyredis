8de7722c2e874ee71c515c811bba504dd198b89e
oyzh <oyzh.1994@qq.com>
2025-08-07 10:08:07 +0800

    1. 细节调整

--- src/main/java/cn/oyzh/easyredis/query/RedisQueryPromptPopup.java
+++ src/main/java/cn/oyzh/easyredis/query/RedisQueryPromptPopup.java
@@ -283,7 +283,7 @@ public class RedisQueryPromptPopup extends FXPopup {
         RenderService.submitFXLater(() -> {
             Optional<Bounds> optional = area.getCaretBounds();
             // 显示提示词
-            optional.ifPresent(value -> this.show(area, value.getCenterX() - 15, value.getCenterY() + 10));
+            optional.ifPresent(value -> this.show(area, value.getCenterX() - 20, value.getCenterY() + 20));
         });
     }
 
