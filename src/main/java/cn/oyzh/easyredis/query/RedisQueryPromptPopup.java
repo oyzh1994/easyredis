49dabc1ad15cf22a78674292e64444ca06be9aaa
oyzh <oyzh.1994@q.qcom>
2025-08-11 10:33:17 +0800

    1. 细节调整

--- src/main/java/cn/oyzh/easyredis/query/RedisQueryPromptPopup.java
+++ src/main/java/cn/oyzh/easyredis/query/RedisQueryPromptPopup.java
@@ -297,13 +297,14 @@ public class RedisQueryPromptPopup extends FXPopup {
     /**
      * 执行自动完成
      *
-     * @param area 文本域
-     * @param item 提示内容
+     * @param editor 编辑器
+     * @param item   提示内容
      */
-    public void autoComplete(RedisQueryEditorPane area, RedisQueryPromptItem item) {
+    public void autoComplete(RedisQueryEditorPane editor, RedisQueryPromptItem item) {
         try {
             if (this.token != null) {
-                area.replaceText(this.token.getStartIndex(), this.token.getEndIndex(), item.getContent());
+                editor.replaceText(this.token.getStartIndex(), this.token.getEndIndex(), item.getContent());
+                editor.positionCaret(this.token.getEndIndex() + item.getContent().length() - 1);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
